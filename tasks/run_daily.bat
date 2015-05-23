d43db9fc27a489d5270636981058c7314e5d4eb1
phi2039 <climber2039@gmail.com>
2015-05-23 10:08:53 -0400

    And another...

--- tasks/run_daily.bat
+++ tasks/run_daily.bat
@@ -1,3 +1,3 @@
 call set_env.bat
 call %PDI_PATH%\kitchen.bat -file:%CD%\master\run_daily.kjb "-param:k84path=z:\Input" -logfile:%CD%\tasks\daily.log
-call %PDI_PATH%\kitchen.bat -file:%CD%\master\run_daily.kjb "-param:recipEmail=clance@livingstonintl.com"
+call %PDI_PATH%\kitchen.bat -file:%CD%\master\send_log.kjb "-param:recipEmail=clance@livingstonintl.com"
