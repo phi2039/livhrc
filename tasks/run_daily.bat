d74e1815c3e5bdc4d48a98b605f44db6ae6294d6
phi2039 <climber2039@gmail.com>
2015-05-25 13:19:18 -0400

    Change log file name

--- tasks/run_daily.bat
+++ tasks/run_daily.bat
@@ -1,3 +1,3 @@
 call set_env.bat
-call %PDI_PATH%\kitchen.bat -file:%CD%\master\run_daily.kjb "-param:k84path=z:\Input" -logfile:%CD%\tasks\daily.log
+call %PDI_PATH%\kitchen.bat -file:%CD%\master\run_daily.kjb "-param:k84path=z:\Input" -logfile:%CD%\tasks\daily_log.txt
 call %PDI_PATH%\kitchen.bat -file:%CD%\master\send_log.kjb "-param:recipEmail=clance@livingstonintl.com"
