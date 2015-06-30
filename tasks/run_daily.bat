2733f46f034e77d2f0922420924258efef27eba5
phi2039 <chris_lance@sbcglobal.net>
2015-06-30 17:12:39 -0400

    Refinements

--- tasks/run_daily.bat
+++ tasks/run_daily.bat
@@ -1,4 +1,4 @@
 call set_env.bat
 call %PDI_PATH%\kitchen.bat -file:%CD%\master\run_daily.kjb "-param:k84path=z:\Input" -logfile:%CD%\tasks\daily_log.txt
-call %PDI_PATH%\kitchen.bat -file:%CD%\master\send_log.kjb "-param:recipEmail=clance@livingstonintl.com"
+REM call %PDI_PATH%\kitchen.bat -file:%CD%\master\send_log.kjb "-param:recipEmail=clance@livingstonintl.com"
 call %PDI_PATH%\kitchen.bat -file:%CD%\util\export\run_exports.kjb "-param:destPath=%CD%\util\export\results"
